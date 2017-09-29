0db8465d9bab4f8347a1359b497d515b13b3833a
karbulot <adam2070@gmail.com>
2017-09-29 15:26:12 +0200

    Initial commit

---
+++ src/main/scala/Trie.scala
@@ -0,0 +1,59 @@
+object Trie {
+    val ALPHABET_LENGTH: Int = 35
+    var root: TrieNode = new TrieNode('\0')
+    def addWord(word: String): Unit = root.addWord(word)
+    def getWords: List[String] = root.getWords
+    class TrieNode(character: Char) {
+        private var parent: TrieNode = _
+        var children: Array[TrieNode] = new Array[TrieNode](ALPHABET_LENGTH)
+        private var isLeaf: Boolean = true
+        private var isWord: Boolean = false
+
+        def addWord(word: String): Unit = {
+            isLeaf = false
+            var charPos: Int = getLetterNumber(word.charAt(0))
+            if (children(charPos) == null) {
+                children(charPos) = new TrieNode(word.charAt(0))
+                children(charPos).parent = this
+            }
+            if (word.length() > 1) {
+                children(charPos).addWord(word.tail)
+            }
+            else {
+                children(charPos).isWord = true
+            }
+        }
+
+        def getWords: List[String] = {
+            var ret: List[String] = List[String]()
+            if (isWord) ret ::= this.toString
+            if (!isLeaf) children.filter(_ != null).foreach(ret :::= _.getWords)
+            ret
+        }
+
+        def getWordsByPrefix(prefix: String): List[String] ={
+            ???
+        }
+
+        override def toString: String = {
+            if (parent == null) ""
+            else parent + character.toString
+        }
+
+        def getWord: String = toString
+
+    }
+
+    def getLetterNumber(c: Char): Int = c match {
+        case 'ą' => 26
+        case 'ć' => 27
+        case 'ę' => 28
+        case 'ł' => 29
+        case 'ń' => 30
+        case 'ó' => 31
+        case 'ś' => 32
+        case 'ż' => 33
+        case 'ź' => 34
+        case _ => c - 'a'
+    }
+}
---
+++ src/main/scala/main.scala
@@ -0,0 +1,19 @@
+object main{
+    def main(args: Array[String]):Unit = {
+        //Trie.clear
+        Trie addWord "a"
+        Trie addWord "b"
+        Trie addWord "ba"
+        Trie addWord "ab"
+        Trie addWord "ac"
+        Trie addWord "abc"
+        Trie addWord "abd"
+        Trie addWord "abcd"
+        Trie addWord "acb"
+        /*
+        Trie addWord "duma"
+        Trie addWord "c"
+        */
+        println(Trie.getWords)
+    }
+}
